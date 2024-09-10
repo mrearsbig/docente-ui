72e05df25bc75e38c0adb97134380f8723c3eb74
Alexander Viafara <mrearsbig@gmail.com>
2024-09-10 09:23:37 -0500

    Agrega las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { ListaDocenteComponent } from './caracteristicas/docentes/componentes/lista-docente/lista-docente.component';
+import { FormularioDocenteComponent } from './caracteristicas/docentes/componentes/formulario-docente/formulario-docente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'docentes',
+    title: 'Docentes',
+    component: ListaDocenteComponent,
+  },
+  {
+    path: 'docentes/crear',
+    title: 'Crear Docente',
+    component: FormularioDocenteComponent,
+  },
+  {
+    path: 'docentes/:id/actualizar',
+    title: 'Actualizar Docente',
+    component: FormularioDocenteComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'docentes',
+  },
+];
