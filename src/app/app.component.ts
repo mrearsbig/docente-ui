e29906ddd4d2edfbd1a05a0266d6908b1e9235dd
Alexander Viafara <mrearsbig@gmail.com>
2024-09-13 21:16:16 -0500

    Actualiza el componente app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from "./compartido/componentes/cabecera/cabecera.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CabeceraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
